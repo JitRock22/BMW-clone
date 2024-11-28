a4b38744b161c53d7c659e3915c04b9552bee544
JitRock22 <jitdas177883@gmail.com>
2024-11-28 18:50:26 +0530

    Second

--- src/components/BasicExample.jsx
+++ src/components/BasicExample.jsx
@@ -1,43 +1,29 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+/* eslint-disable no-unused-vars */
+import React from 'react'
 
-function BasicExample() {
+const BasicExample = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary p-[20px] h-[80px] m-[100px] mt-[50px] justify-center" bg="light" data-bs-theme="light">
-      <Container>
-        <Navbar.Brand href="#home" className='flex justify-evenly'>
-          <img src="https://www.bmw.com/etc.clientlibs/settings/wcm/designs/bmwcom/base/resources/ci2020/img/logo-bmw-com-gray.svg" alt="" />
-          <span className=' m-[10px]'>| Sheer Driving Pleasure</span>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto ml-[100px]">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+    <>
+      <div className=' p-[20px] m-[40px]  top-0'>
+        <div className=' bg-white'>
+          <ul className='nv1'>
+            <li><img src="https://www.bmw.com/etc.clientlibs/settings/wcm/designs/bmwcom/base/resources/ci2020/img/logo-bmw-com-gray.svg" alt="" className=' p-4' /></li>
+            <li><h3 className='text-[20px] font-[f1]'>|  Sheer Driving Pleasure</h3></li>
+
+            <div className='flex nav '>
+              <li><a href="">Home</a></li>
+              <li><a href="">Circular World</a></li>
+              <li><a href="">Digital Journey</a></li>
+              <li><a href="">Electric Future</a></li>
+              <li><a href="">Models</a></li>
+            </div>
+          </ul>
+        </div>
+
+      </div>
+    </>
+
+  )
 }
 
-export default BasicExample;
+export default BasicExample
--- src/components/Page1.jsx
+++ src/components/Page1.jsx
@@ -1,26 +1,55 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import bmw1 from '../assets/bmw1.mp4'
+import bmw2 from '../assets/BMW2.mp4'
+import bmw3 from '../assets/bmw3.mp4'
 import BasicExample from './BasicExample'
 const Page1 = () => {
     return (
         <>
             <div className='main'>
                 <video autoPlay loop muted>
-                    <source  src={bmw1} type='video/mp4'/>
+                    <source src={bmw1} type='video/mp4' />
                 </video>
-               <div className='content'>
-                <BasicExample/>
-               </div>
-               <div className=' text-[50px] font-[f1] p-[10px] ml-[100px] mt-[200px] position-absolute top-1 text-white'>
-                <h1>The magic of exclusive moments</h1>
-                <div className='text-[20px] p-[50px]'>
-                <button className='w-[150px] h-[50px] p-[10px] border-white border-2 rounded-none text-center'>Read More</button>
+                <div className='content'>
+                    <BasicExample />
                 </div>
-                    
-               </div>
+                <div className='m1 absolute top-0  text-[50px] font-[f1]'>
+                    <h1>BMW M5</h1>
+                    <h2>The magic of exclusive moments</h2>
+                    <button className='text-[20px] p-[18px] mt-[15px] border-2'>READ MORE</button>
+                </div>
             </div>
-            
+            <div className='m2'>
+                <div className='m3'>
+                    {/* <img src="https://www.bmw.com/content/dam/bmw/marketBMWCOM/bmw_com/categories/automotive-life/bmwapp-wallpaper/bawp-00-teaser-high.jpg?imwidth=360" alt="" /> */}
+                    <video autoPlay loop muted>
+                        <source src={bmw2} type='video/mp4' />
+                    </video>
+                </div>
+                <div className='m3'>
+                    {/* <img src="https://www.bmw.com/content/dam/bmw/marketBMWCOM/bmw_com/categories/automotive-life/bmwapp-wallpaper/bawp-00-teaser-high.jpg?imwidth=360" alt="" /> */}
+                    <div>
+                        <h1 className='text-[50px] mt-[90px] font-[f1]'>Performance Redefined:<br />
+                            <span>Drive the <b className='font-[f1]'>BMW M3<br /><button className='text-[20px] p-[18px] mt-[15px] border-2 border-black'>BOOK NOW</button> </b></span></h1>
+
+                    </div>
+                </div>
+            </div>
+            <div className='m2'>
+                <div className='m3'>
+                    <h1 className='text-[45px] mt-[90px] font-[f1]'>Revolutionizing Performance:<br />
+                        <span className=' text-center'> The Legendary  <br /><b className='font-[f1]'>BMW M4<br /><button className='text-[20px] p-[18px] mt-[15px] border-2 border-black'>BOOK NOW</button> </b></span></h1>
+
+
+                </div>
+                <div className='m3'>
+                    <video autoPlay loop muted>
+                        <source src={bmw3} type='video/mp4' />
+                    </video>
+                </div>
+            </div>
+
 
         </>
 
